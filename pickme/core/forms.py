dca9d0d82fa704a950cd8c6eb744d687f9daad3e
highb33kay <hiibeekayvibe@gmail.com>
2022-09-04 12:34:37 +0100

    added super admin successfully

--- pickme/core/forms.py
+++ pickme/core/forms.py
@@ -8,4 +8,11 @@ class CustomUserCreationForm(UserCreationForm):
     # custom user model
     class Meta(UserCreationForm):
         model = User
-        fields = ('username', 'email', 'password1', 'password2', 'gender')
+        fields = ('username', 'email', 'password1', 'password2')
+        
+class CustomUserChangeForm(UserChangeForm):
+    
+    # custom user model
+    class Meta:
+        model = User
+        fields = ('username', 'email')
--- pickme/core/models.py
+++ pickme/core/models.py
@@ -1,10 +1,12 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 # create a sex option 
 
 
 # Create your models here.
 # create a user model
-class User(models.Model):
+class User(AbstractUser):
+#    Creating a sex option for the user.
     MALE = 'M'
     FEMALE = 'F'
     NONBINARY = 'NB'
@@ -13,7 +15,7 @@ class User(models.Model):
         (FEMALE, 'Female'),
         (NONBINARY, 'Non-Binary')
     ]
-    username = models.CharField(max_length=100)
+    username = models.CharField(max_length=100,unique=True)
     email = models.EmailField(max_length=100)
     password = models.CharField(max_length=100)
     gender = models.CharField(
@@ -21,45 +23,45 @@ class User(models.Model):
         choices=GENDER,
         default=FEMALE,
     )
-
+    pass
 
     def __str__(self):
         return self.username
     
-# create a pick model
-class PickUpLine(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    line = models.CharField(max_length=100)
-    likes = models.IntegerField(default=0)
-    gender = models.ForeignKey(User, on_delete=models.CASCADE)
-    # sort by gender
+# # create a pick model
+# class PickUpLine(models.Model):
+#     user = models.ForeignKey(User, on_delete=models.CASCADE)
+#     line = models.CharField(max_length=100)
+#     likes = models.IntegerField(default=0)
+#     gender = models.ForeignKey(User, on_delete=models.CASCADE)
+#     # sort by gender
 
-    def __str__(self):
-        return self.line
+#     def __str__(self):
+#         return self.line
     
-    # sort by gender
+#     # sort by gender
 
     
-class Like(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    pick = models.ForeignKey(PickUpLine, on_delete=models.CASCADE)
-    like = models.BooleanField(default=False)
+# class Like(models.Model):
+#     user = models.ForeignKey(User, on_delete=models.CASCADE)
+#     pick = models.ForeignKey(PickUpLine, on_delete=models.CASCADE)
+#     like = models.BooleanField(default=False)
 
-    def __str__(self):
-        return self.like
+#     def __str__(self):
+#         return self.like
 
-class Dislike(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    pick = models.ForeignKey(PickUpLine, on_delete=models.CASCADE)
-    dislike = models.BooleanField(default=False)
+# class Dislike(models.Model):
+#     user = models.ForeignKey(User, on_delete=models.CASCADE)
+#     pick = models.ForeignKey(PickUpLine, on_delete=models.CASCADE)
+#     dislike = models.BooleanField(default=False)
 
-    def __str__(self):
-        return self.dislike
+#     def __str__(self):
+#         return self.dislike
     
-class Comment(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    pick = models.ForeignKey(PickUpLine, on_delete=models.CASCADE)
-    comment = models.CharField(max_length=100)
+# class Comment(models.Model):
+#     user = models.ForeignKey(User, on_delete=models.CASCADE)
+#     pick = models.ForeignKey(PickUpLine, on_delete=models.CASCADE)
+#     comment = models.CharField(max_length=100)
 
-    def __str__(self):
-        return self.comment
+#     def __str__(self):
+#         return self.comment
